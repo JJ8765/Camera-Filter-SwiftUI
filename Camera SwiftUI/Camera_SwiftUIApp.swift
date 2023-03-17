64d28a49c5b375f332d33c478295e5084cc37c34
Jaden <minijaden8765@gmail.com>
2023-03-17 09:44:28 -0700

    .

---
+++ Camera SwiftUI/Camera_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Camera_SwiftUIApp.swift
+//  Camera SwiftUI
+//
+//  Created by Jaden Lin on 3/16/23.
+//
+
+import SwiftUI
+
+@main
+struct Camera_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
